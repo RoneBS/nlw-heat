8a7f26a65fefd1e3eeb4ad3b3112e68bb4d825d0
Rone Silveira <ronebs10@hotmail.com>
2021-10-20 09:35:01 -0300

    authenticating user receiving code, created

--- node_heat/src/services/AuthenticateUserService.ts
+++ node_heat/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { prismaClient } from '../prisma'
+import { sign } from 'jsonwebtoken'
 
 interface IAccessTokenResponse {
   access_token: string
@@ -51,8 +52,23 @@ class AuthenticateUserService {
         }
       })
     }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_ur: user.avatar_url,
+          id: user.id
+        }
+    },
+    process.env.JWT_SECRET,
+    {
+      subject: user.id,
+      expiresIn: "1d"
+    }
+    )
     
-    return response.data
+    return { token, user }
   }
 }
 
