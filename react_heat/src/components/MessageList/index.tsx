e2c00ebba30b9e2c8309e8dab4819e82e3c29d84
Rone Silveira <ronebs10@hotmail.com>
2021-10-21 15:45:36 -0300

    MessageList component and styles done

--- react_heat/src/components/MessageList/index.tsx
+++ react_heat/src/components/MessageList/index.tsx
@@ -1,5 +1,52 @@
+import * as S from './styles'
+
+import logoImg from '../../assets/logo.svg'
+
 export const MessageList = () => {
   return(
-    <h1>MessageList</h1>
+    <S.MessageListWrapper>
+      <img src={logoImg} alt="DoWhile 2021" />
+      <S.MessageList>
+        <S.Message>
+          <S.MessageContent>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </S.MessageContent>
+          <S.MessageUser>
+            <S.UserImage>
+              <img src="https://github.com/RoneBS.png" alt="Rone Silveira" />
+            </S.UserImage>
+            <span>Rone Silveira</span>
+          </S.MessageUser>
+        </S.Message>
+
+        <S.Message>
+          <S.MessageContent>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </S.MessageContent>
+          <S.MessageUser>
+            <S.UserImage>
+              <img src="https://github.com/RoneBS.png" alt="Rone Silveira" />
+            </S.UserImage>
+            <span>Rone Silveira</span>
+          </S.MessageUser>
+        </S.Message>
+
+        <S.Message>
+          <S.MessageContent>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </S.MessageContent>
+          <S.MessageUser>
+            <S.UserImage>
+              <img src="https://github.com/RoneBS.png" alt="Rone Silveira" />
+            </S.UserImage>
+            <span>Rone Silveira</span>
+          </S.MessageUser>
+        </S.Message>
+      </S.MessageList>
+      
+    </S.MessageListWrapper>
   )
 }
