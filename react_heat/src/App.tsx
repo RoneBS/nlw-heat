6a5c764e4688b291da38306c7008aa4143a1b81b
Rone Silveira <ronebs10@hotmail.com>
2021-10-22 15:50:56 -0300

    AuthContext added to App.tsx for render SendMessageForm if user is logged, and LoginBox if not

--- react_heat/src/App.tsx
+++ react_heat/src/App.tsx
@@ -1,12 +1,17 @@
+import { useContext } from 'react'
 import * as S from './App.styles'
 import { LoginBox } from './components/LoginBox'
 import { MessageList } from './components/MessageList'
+import { SendMessageForm } from './components/SendMessageForm'
+import { AuthContext } from './contexts/auth'
 
 export function App() {
+  const { user } = useContext(AuthContext)
+
   return (
     <S.ContentWrapper>
       <MessageList />
-      <LoginBox />
+      { !!user ? <SendMessageForm /> : <LoginBox />}
     </S.ContentWrapper>
    
   )
