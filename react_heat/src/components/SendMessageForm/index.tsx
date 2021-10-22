c5497e8246efcf2964313c2ece2b1b5e564260bb
Rone Silveira <ronebs10@hotmail.com>
2021-10-22 15:47:08 -0300

    structure and styles added to SendMessageForm component

--- react_heat/src/components/SendMessageForm/index.tsx
+++ react_heat/src/components/SendMessageForm/index.tsx
@@ -1,5 +1,38 @@
+import { useContext } from 'react'
+import { VscGithubInverted, VscSignOut } from 'react-icons/vsc'
+import { AuthContext } from '../../contexts/auth'
+import * as S from './styles'
+
 export const SendMessageForm = () => {
+  const { user } = useContext(AuthContext)
+
   return(
-    <h1>SendMessageForm</h1>
+   <S.SendMessageFormWrapper>
+     <S.SignOutButton>
+       <VscSignOut size="32" />
+     </S.SignOutButton>
+
+     <S.UserInformation>
+       <S.UserImage>
+         <img src={user?.avatar_url} alt={user?.name} />
+       </S.UserImage>
+       <S.UserName>{user?.name}</S.UserName>
+       <S.UserGithub>
+         <VscGithubInverted size="16" />
+         {user?.login}
+       </S.UserGithub>
+     </S.UserInformation>
+
+     <S.SendMessageForm>
+       <label htmlFor="message">Mensagem</label>
+       <textarea 
+         name="message" 
+         id="message" 
+         placeholder="Qual a sua expectativa para o evento?"
+       />
+
+       <button type="submit">Enviar mensagem</button>
+     </S.SendMessageForm>
+   </S.SendMessageFormWrapper>
   )
 }
