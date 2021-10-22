5e876c74b0d2e18af9e194dfbc73526850dc0b43
Rone Silveira <ronebs10@hotmail.com>
2021-10-22 14:53:03 -0300

    context AuthContext created to send authentication data to other components

--- react_heat/src/components/LoginBox/index.tsx
+++ react_heat/src/components/LoginBox/index.tsx
@@ -1,48 +1,11 @@
-import { useEffect } from 'react'
+import { useContext } from 'react'
 import { VscGithubInverted } from 'react-icons/vsc'
-import { api } from '../../services/api'
+import { AuthContext } from '../../contexts/auth'
 
 import * as S from './styles'
 
-type AuthResponse = {
-  token: string;
-  user: {
-    id: string;
-    avatar_url: string;
-    name: string;
-    login: string;
-  }
-}
-
 export const LoginBox = () => {
-  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=fc47c9cc8481ec3837f7`
-
-  async function signIn(githubCode: string) {
-    const response = await api.post<AuthResponse>('authenticate', {
-      code: githubCode,
-    })
-
-    const { token, user } = response.data
-
-    localStorage.setItem('@dowhile:token', token)
-
-    console.log(user)
-  }
-
-  useEffect(() => {
-    const url = window.location.href
-    const hasGithubCode = url.includes('?code=')
-
-    if (hasGithubCode) {
-      const [urlWithoutCode, githubCode] = url.split('?code=')
-
-      // console.log({ urlWithoutCode, githubCode })
-
-      window.history.pushState({}, '', urlWithoutCode)  
-      
-      signIn(githubCode)
-    }
-  }, [])
+  const { signInUrl } = useContext(AuthContext)
 
   return(
     <S.LoginBoxWrapper>
--- react_heat/src/main.tsx
+++ react_heat/src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import { AuthProvider } from './contexts/auth'
 import { GlobalStyle } from './styles/global'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <GlobalStyle />
+    <AuthProvider>
+      <App />
+      <GlobalStyle />
+    </AuthProvider>
+    
   </React.StrictMode>,
   
   document.getElementById('root')
