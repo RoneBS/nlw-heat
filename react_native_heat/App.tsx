97d1f6de1e0b4a80b511089685ef25d676727fdf
Rone Silveira <ronebs10@hotmail.com>
2021-10-24 14:18:55 -0300

    google fonts and expo-app-loading added

--- react_native_heat/App.tsx
+++ react_native_heat/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Home } from './src/screens/Home'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
   return (
     <Home />
   );
