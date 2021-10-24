27bc675b1853ebbdce90ef619989be0564a5b94d
Rone Silveira <ronebs10@hotmail.com>
2021-10-24 15:10:44 -0300

    svg config metro.config.js and status bar added

--- react_native_heat/App.tsx
+++ react_native_heat/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 import { Home } from './src/screens/Home'
 
 export default function App() {
@@ -7,8 +9,16 @@ export default function App() {
     Roboto_400Regular,
     Roboto_700Bold
   })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
   return (
-    <Home />
+    <>
+      <StatusBar style='light'/>
+       <Home />
+    </>
+   
   );
 }
 
