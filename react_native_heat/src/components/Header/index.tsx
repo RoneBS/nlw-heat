a4667d78c3a09e7173b0f909ff5a901feb6456b0
Rone Silveira <ronebs10@hotmail.com>
2021-10-25 14:14:06 -0300

    linear gradient dependencie added, Header component done

--- react_native_heat/src/components/Header/index.tsx
+++ react_native_heat/src/components/Header/index.tsx
@@ -11,12 +11,13 @@ export function Header() {
   return(
     <View style={styles.container}>
       <LogoSvg />
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
 
-      <UserPhoto imageUri='https://github.com/RoneBS.png'/>
-
-      <TouchableOpacity>
-        <Text style={styles.logoutText}>Sair</Text>
-      </TouchableOpacity>
+        <UserPhoto imageUri='https://github.com/RoneBS.png'/>
+      </View>     
     </View>
   )
 }
