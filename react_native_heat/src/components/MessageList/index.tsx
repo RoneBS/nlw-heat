fa4854f4f8166499d4288efc3440962f756ef381
Rone Silveira <ronebs10@hotmail.com>
2021-10-27 15:20:46 -0300

    rendering backend data, user posted messages

--- react_native_heat/src/components/MessageList/index.tsx
+++ react_native_heat/src/components/MessageList/index.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ScrollView } from 'react-native'
+import { api } from '../../services/api'
 
-import { Message } from '../Message'
+import { Message, MessageProps } from '../Message'
 
 import { styles } from './styles'
 
-const message = {
-  id: '1',
-  text: 'mensagem de teste',
-  user: {
-    name: 'Rone',
-    avatar_url: 'https://github.com/RoneBS.png',
-  }
-}
+
 
 export function MessageList() {
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([])
+
+useEffect(() => {
+  async function fetchMessages(){
+    const messagesResponse = await api.get<MessageProps[]>('messages/last3')
+    setCurrentMessages(messagesResponse.data)
+  }
+
+  fetchMessages()
+}, [])
+
   return (
     <ScrollView 
       style={styles.container}
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
       >
-      <Message data={message}/>
-      <Message data={message}/>
-      <Message data={message}/>
+      {currentMessages.map((message) => <Message key={message.id} data={message}/>)}
+      
     </ScrollView>
   )
 }
